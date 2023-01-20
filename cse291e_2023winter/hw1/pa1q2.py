283c825cf54a28722596c89c459cb94fbaacc2f2
samuelfujie <samuel.fujie@gmail.com>
2023-01-20 12:50:40 -0800

    Initialize task 2

---
+++ cse291e_2023winter/hw1/pa1q2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+=============================================================================
+Created By  : Samuel Fu
+Created Date: Fri Jan 20 11:31:54 PST 2023
+Class: CSE 291E
+Professor: KC Claffy
+=============================================================================
+'''
+
+import json
+import requests
+
